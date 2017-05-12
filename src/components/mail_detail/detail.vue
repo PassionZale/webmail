951797b13d438a3f63f4c227cb789630c664924b
whouu <whouu@qq.com>
2017-05-12 10:41:33 +0800

    更新：邮件详情

--- src/components/mail_detail/detail.vue
+++ src/components/mail_detail/detail.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="mdui-toolbar mdui-color-theme">
-        <a @click.prevent="goBack" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">&#xe5c4;</i></a>
-        <span class="mdui-typo-title">邮件详情</span>
+
+    <div class="mdui-appbar mdui-appbar-fixed mdui-appbar-scroll-hide">
+        <div class="mdui-toolbar mdui-color-theme-accent">
+            <a @click.prevent="goBack" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">&#xe5c4;</i></a>
+            <span class="mdui-typo-title">邮件详情</span>
+            <div class="mdui-toolbar-spacer"></div>
+            <a href="javascript:">
+                快速回复
+            </a>
+        </div>
     </div>
+
     <div class="mdui-panel" id="panel">
         <div class="mdui-panel-item" id="panel-subject">
             <div class="mdui-panel-item-header">
@@ -45,6 +53,7 @@
             </div>
         </div>
     </div>
+
     <div class="mdui-container">
         <div class="mdui-typo">
             <h4 class="doc-article-title">邮件排版示例 <a class="doc-anchor" id="demo"></a></h4>
